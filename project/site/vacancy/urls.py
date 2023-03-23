3b585e7cc21fbf23d22ac2ab01d1996ecc057136
Abdusamad Dusabaev <abdusamad.dusabaev@yandex.ru>
2023-03-23 21:33:16 +0600

    Add static files

--- project/site/vacancy/urls.py
+++ project/site/vacancy/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from .settings import STATIC_ROOT, STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls"))
-]
+].extend(static(STATIC_URL, document_root=STATIC_ROOT))
